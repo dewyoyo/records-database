27798d3bea035e8e8020cf81163e7bfdb0ff669c
dewyoyo <dewyoyo@hotmail.com>
2019-01-15 08:00:17 -0700

    window.load

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+window.onload = function(){ 
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function (data) {
@@ -20,7 +20,32 @@ $(document).ready(function () {
     }
 
   });
+}
+
+
+// $(document).ready(function () {
+//   // This file just does a GET request to figure out which user is logged in
+//   // and updates the HTML on the page
+//   $.get("/api/user_data").then(function (data) {
+//     // $(".member-name").text(data.email);
+
+
+//     if (data.email) {
+//       var newHref = "<div class='navbar-header'> "
+//         + "Logged as &lt;" + data.email + "&gt;"
+//         + " <a class='navbar-brand' id='data-welcome' href='/logout'>Logout</a> "
+//         + "</div>";
+//       $("#header").append(newHref);
+//     }
+//     else {
+//       var newHref = "<div class='navbar-header'> "
+//       + " <a class='navbar-brand' id='data-welcome' href='/login'>Login</a> "
+//       + "</div>";
+//     $("#header").append(newHref);
+//     }
+
+//   });
 
 
 
-});
+// });
