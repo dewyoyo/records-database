06d3f7820a2024b1cc3bb8651e4276b87f9e9ffe
dewyoyo <dewyoyo@hotmail.com>
2019-01-14 21:01:00 -0700

    change port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 
 
